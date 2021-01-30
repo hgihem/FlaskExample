da3127b11c272427db3aa90dbbafbaa137395a29
Mihai Turdean <6640685+hgihem@users.noreply.github.com>
2021-01-30 11:56:57 -0600

    FIx database_setup according to PIP8

--- database_setup.py
+++ database_setup.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
 db = SQLAlchemy(app)
 
+
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return "<Restaurant(\n\tname='%s',\n\tid='%i'\n)>" % (self.name, self.id)
@@ -16,8 +17,8 @@ class Restaurant(db.Model):
 
 class MenuItem(db.Model):
     __tablename__ = 'menu_item'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
     course = db.Column(db.String(250))
     description = db.Column(db.String(250))
     price = db.Column(db.String(8))
@@ -26,16 +27,18 @@ class MenuItem(db.Model):
 
     def serialize(self):
         return {
-            'name'          :self.name,
-            'description'   :self.description,
-            'id'            :self.id,
-            'price'         :self.price,
-            'course'        :self.course,
+            'name':         self.name,
+            'description':  self.description,
+            'id':           self.id,
+            'price':        self.price,
+            'course':       self.course,
         }
 
     def __repr__(self):
-        return "<MenuItem(\n\tname='%s',\n\tid='%i',\n\tcourse='%s',\n\tdescription='%s',\n\tprice='%s',\n\trestaurant_id='%s'\n)>" % (
-            self.name, self.id, self.course, self.description, self.price, self.restaurant_id)
+        return "<MenuItem(\n\tname='%s',\n\tid='%i',\n\tcourse='%s',\
+            \n\tdescription='%s',\n\tprice='%s',\n\trestaurant_id='%s'\n)>" % (
+            self.name, self.id, self.course, self.description,
+            self.price, self.restaurant_id)
 
 
 db.create_all()
